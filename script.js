19280d6a3be66fac7694c48f9ddbdb27a0cf6832
Alan-Velozo <113318689+Alan-Velozo@users.noreply.github.com>
2024-04-06 20:59:25 -0300

    scroll animations

--- script.js
+++ script.js
@@ -30,3 +30,54 @@ window.onscroll = function(){
     searchForm.classList.remove("active");
     deliveryItem.classList.remove("active");
 }
+
+                                                /* Scroll */
+
+document.addEventListener("scroll", function() {
+    var divs = document.querySelectorAll(".left");
+                                                
+    divs.forEach(function(div) {
+        var rect = div.getBoundingClientRect();
+                                                
+        if (rect.top <= window.innerHeight * 0.75) {
+            div.style.opacity = "1";
+            div.style.transform = "translateX(0)";
+        } else {
+            div.style.opacity = "0";
+            div.style.transform = "translateX(-100%)";
+        }
+    });
+});
+                                                
+document.addEventListener("scroll", function() {
+    var divs = document.querySelectorAll(".right");
+                                                
+    divs.forEach(function(div) {
+        var rect = div.getBoundingClientRect();
+                                                
+        if (rect.top <= window.innerHeight * 0.75) {
+            div.style.opacity = "1";
+            div.style.transform = "translateX(0)";
+        } else {
+            div.style.opacity = "0";
+            div.style.transform = "translateX(200%)";
+        }
+    });
+});
+                                                
+document.addEventListener("scroll", function() {
+    var divs = document.querySelectorAll(".down");
+                                                
+    divs.forEach(function(div) {
+        var rect = div.getBoundingClientRect();
+                                                
+        if (rect.top <= window.innerHeight * 0.75) {
+            div.style.opacity = "1";
+            div.style.transform = "translateY(0)";
+        } else {
+            div.style.opacity = "0";
+            div.style.transform = "translateY(50%)";
+        }
+    });
+});
+                                                
